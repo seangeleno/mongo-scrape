efd8192ea9e774c4bfad0dee5d27fe582e7bc773
g wilken <greg@gwilken.com>
2017-06-03 12:21:00 -0700

    price scrapper and parsing working

--- controllers/routes.js
+++ controllers/routes.js
@@ -7,7 +7,31 @@ const db = require('../models/mongo.js');
 
 var router = function(app) {
 
+  app.get("/scrape", function(req, res) {
 
+    console.log('scrape hit.');
+
+    request("https://losangeles.craigslist.org/search/mca?query=honda+xr", function(error, response, html) {
+
+      var $ = cheerio.load(html);
+
+      var result = [];
+
+      $("ul.rows li a span.result-price").each(function(i, element) {
+
+        var price = $(this).text().replace(/\$/g, '');
+
+
+        result.push( parseFloat(price) );
+
+      });
+
+          res.send(result);
+
+    });
+
+
+  });
 
 }
 
